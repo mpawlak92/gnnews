917522754aec1792adb8b5f4d83f6ce1c717ca57
Michal Pawlak <mpawlak.it@gmail.com>
2023-03-31 23:29:34 +0200

    added content to author modal

--- src/components/InfoBtn/InfoBtn.jsx
+++ src/components/InfoBtn/InfoBtn.jsx
@@ -21,18 +21,15 @@ const InfoBtn = () => {
         <div className="info-popup-content">
           <h2 className="info-popup-header">The coolest:</h2>
           <p className="info-popup-text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque
-            accusamus maiores reprehenderit ex culpa dignissimos incidunt
-            accusantium, esse repellendus dolorem totam, pariatur quidem
-            inventore provident. Officiis accusantium perspiciatis veniam
-            nostrum.
+            What I liked the most was that I could finally write my modal from
+            scratch, I did not have time for it before.
           </p>
           <h2 className="info-popup-header--red">Difficulties:</h2>
           <p className="info-popup-text">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum non
-            numquam, officiis quam vero autem ratione doloribus ea ipsum, id
-            blanditiis saepe est architecto, provident minima dolore sunt
-            aliquid nisi?
+            The task was quite simple, I had done everything before. Although,
+            due to my affairs, I did not have much time to refine them. What
+            caused me the most difficulties was the side menu, especially
+            adjusting it to smaller screens, e.g. a 14 inch display in full hd.
           </p>
         </div>
         <div className="info-popup-thanks">
