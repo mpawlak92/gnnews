806ea4e0a83ceec46489d00cadc93a647192ca32
Michal Pawlak <mpawlak.it@gmail.com>
2023-03-31 20:47:07 +0200

    created header menu

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -1,11 +1,15 @@
 import './Header.scss';
 import Logo from '../../components/Logo/Logo';
 import ChangeArticlesViewBtn from '../../components/ChangeArticlesViewBtn/ChangeArticlesViewBtn';
+import InfoBtn from '../../components/InfoBtn/InfoBtn';
 const Header = () => {
   return (
     <div className="header">
       <Logo />
-      <ChangeArticlesViewBtn />
+      <div className='header__buttons'>
+        <InfoBtn />
+        <ChangeArticlesViewBtn />
+      </div>
     </div>
   );
 };
